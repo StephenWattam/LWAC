4b0b5c6129fd857d2e0cfe78a58037f7dddb8ff2
Steve Wattam <stephenwattam@gmail.com>
2013-04-22 13:11:44 +0100

    Added module file lib/lwac

---
+++ lib/lwac.rb
@@ -0,0 +1,15 @@
+
+# LWAC namespace
+module LWAC
+  # Overall LWAC version, as in the git tags
+  VERSION = "0.2.0b"
+  # Date of last significant edit
+  DATE = "21-04-13"
+
+  # Authors
+  AUTHORS = [
+              {:name => "Stephen Wattam", :contact => "http://stephenwattam.com"},
+             #{:name => "", :contact => ""}  # Add yourself here (and in the gemspec) if you contribute to LWAC
+            ]
+
+end
